f7551aec03155ada5ebb290644425df781a126ba
bernatto_uec <uec.tsje@gmail.com>
2022-07-05 14:50:53 -0400

    css v7

--- pages/index.js
+++ pages/index.js
@@ -52,15 +52,41 @@ export default function Home() {
             <Page pageNumber={pageNumber} />
           </Document>
 
-          <div className='absolute top-5 rounded-full shadow-md bg-white flex gap-1'>
-            <button className='bg-white flex flex-col items-center font-bold w-5 rounded-l-full hover:bg-gray-300'>
-              ‹
+          <div className='absolute top-5 rounded-full shadow-md bg-white dark:bg-gray-700 flex gap-1 h-10'>
+            <button className='font-bold w-9 flex justify-center items-center rounded-l-full hover:bg-gray-300 dark:hover:bg-gray-800 text-center'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-6 w-6'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M15 19l-7-7 7-7'
+                />
+              </svg>
             </button>
-            <span className='bg-white'>
+            <div className='flex justify-center items-center'>
               {pageNumber} de {numPages}
-            </span>
-            <button className='bg-white flex flex-col items-center font-bold w-5 rounded-r-full hover:bg-gray-300'>
-              ›
+            </div>
+            <button className='font-bold w-9 flex items-center justify-center rounded-r-full hover:bg-gray-300 dark:hover:bg-gray-800 text-center'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-6 w-6'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M9 5l7 7-7 7'
+                />
+              </svg>
             </button>
           </div>
         </div>
