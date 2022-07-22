5e61f4ff4b042907d3acca22fd691c75bf08d054
bernatto_uec <uec.tsje@gmail.com>
2022-07-22 17:47:58 -0400

    url pdf testing

--- components/PdfDocument.js
+++ components/PdfDocument.js
@@ -31,7 +31,7 @@ const PdfDocument = () => {
             xmlns='http://www.w3.org/2000/svg'
             className='h-7 w-7'
             fill='none'
-            viewBox='0 0 24 24'
+            viewBox='o0 0 24 24'
             stroke='currentColor'
             strokeWidth={2}
           >
@@ -44,11 +44,14 @@ const PdfDocument = () => {
         </button>
       </span>
       <Document
-        file={urlPdf}
+        file={{
+          url: urlPdf
+        }}
         onLoadSuccess={onDocumentLoadSuccess}
         className='rounded-md relative flex justify-center'
         loading={Spinner}
         noData={'No existe PDF asignado'}
+        onLoadError={console.error}
       >
         <Page
           pageNumber={pageNumber}
