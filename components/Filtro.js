82066f46762ff13d85adc7d204aaa7c5733fc6b0
bernatto_uec <uec.tsje@gmail.com>
2022-08-09 16:34:44 -0400

    reset form button added

--- components/Filtro.js
+++ components/Filtro.js
@@ -10,23 +10,15 @@ const Filtro = () => {
   const { years, tipoResoluciones } = AppContext
 
   const filtroSchema = Yup.object().shape({
-    anyText: Yup.string()
-      .nullable(true)
-      .transform((_, val) => (val === Number(val) ? val : null)),
-    resolucion: Yup.number()
-      .nullable(true)
-      .transform((_, val) => (val === Number(val) ? val : null)),
-    year: Yup.number()
-      .nullable(true)
-      .transform((_, val) => (val === Number(val) ? val : null)),
-    tipoResolucion: Yup.number()
-      .nullable(true)
-      .transform((_, val) => (val === Number(val) ? val : null))
+    buscador: Yup.string().nullable(true),
+    nroResolucion: Yup.number().nullable(true),
+    yearResolucion: Yup.number().nullable(true).typeError('Must be a number'),
+    tipoResolucion: Yup.number().nullable(true).typeError('Must be a number')
   })
 
   const checkValues = async valores => {
-    const { buscador, nroResolucion, year, tipoResolucion } = valores
-    if (buscador || nroResolucion || year || tipoResolucion) {
+    const { buscador, nroResolucion, yearResolucion, tipoResolucion } = valores
+    if (buscador || nroResolucion || yearResolucion || tipoResolucion) {
       console.log(valores)
     } else {
       console.log('Al menos carga un valor')
@@ -54,7 +46,7 @@ const Filtro = () => {
       initialValues={{
         buscador: '',
         nroResolucion: '',
-        year: '',
+        yearResolucion: '',
         tipoResolucion: ''
       }}
       onSubmit={async (values, { resetForm }) => {
@@ -63,7 +55,7 @@ const Filtro = () => {
       }}
       validationSchema={filtroSchema}
     >
-      {({ errors, touched, setFieldValue }) => (
+      {({ errors, touched, setFieldValue, resetForm }) => (
         <Form className='w-full max-w-6xl flex flex-wrap gap-3 justify-center shadow-md py-4 px-1 rounded-xl dark:shadow-gray-800 border dark:border-gray-700 text-xs'>
           <span className='flex w-full justify-center items-center'>
             <div className='flex justify-center bg-white dark:border-gray-700 dark:bg-gray-700 items-center shadow-md border border-opacity-75 border-gray-300 rounded-lg px-2 sm:px-3 py-2 dark:shadow-gray-800 mb-5 w-80'>
@@ -118,20 +110,24 @@ const Filtro = () => {
           <div className='select flex justify-center items-center font-semibold p-2 dark:bg-gray-700 shadow-md rounded-xl border max-w-xs relative bg-white dark:border-gray-700 dark:shadow-gray-800'>
             <label
               className='absolute -top-3 font-bold shadow border rounded-md bg-white dark:bg-white dark:text-gray-700 px-2'
-              htmlFor='year'
+              htmlFor='yearResolucion'
             >
               Año
             </label>
             <Field
               className='shadow appearance-none rounded p-2 font-bold focus:outline-none focus:shadow-outline text-xs uppercase bg-transparent border-b-4 border-teal-600 text-center relative px-3'
               as='select'
-              name='year'
-              id='year'
-              onChange={e => setFieldValue('year', +e.currentTarget.value)}
+              name='yearResolucion'
+              id='yearResolucion'
+              onChange={e =>
+                e.currentTarget.value !== ''
+                  ? setFieldValue('yearResolucion', +e.currentTarget.value)
+                  : setFieldValue('yearResolucion', null)
+              }
             >
               <option
                 className='font-bold rounded-md shadow dark:bg-gray-700 appearance-none text-center'
-                value={null}
+                value=''
               >
                 --Seleccionar--
               </option>
@@ -145,7 +141,9 @@ const Filtro = () => {
                 </option>
               ))}
             </Field>
-            {errors.year && touched.year ? <div>{errors.year}</div> : null}
+            {errors.yearResolucion && touched.yearResolucion ? (
+              <div>{errors.yearResolucion}</div>
+            ) : null}
           </div>
 
           <div className='select flex justify-center items-center font-semibold p-2 dark:bg-gray-700 shadow-md rounded-xl border max-w-sm relative bg-white dark:border-gray-700 dark:shadow-gray-800'>
@@ -161,12 +159,14 @@ const Filtro = () => {
               name='tipoResolucion'
               id='tipoResolucion'
               onChange={e =>
-                setFieldValue('tipoResolucion', +e.currentTarget.value)
+                e.currentTarget.value !== ''
+                  ? setFieldValue('tipoResolucion', +e.currentTarget.value)
+                  : setFieldValue('tipoResolucion', null)
               }
             >
               <option
                 className='font-bold rounded-md shadow dark:bg-gray-700 appearance-none text-center'
-                value={null}
+                value=''
               >
                 --Seleccionar--
               </option>
@@ -203,6 +203,31 @@ const Filtro = () => {
                   strokeLinejoin='round'
                   strokeWidth='2'
                   d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
+                />
+              </svg>
+            </button>
+          </div>
+          <div className='w-full flex justify-center items-center font-bold relative'>
+            <button
+              aria-label='button'
+              className='flex justify-center items-center border shadow-md rounded-xl p-2 transition-transform hover:scale-105 bg-white dark:bg-gray-700 dark:border-gray-700 dark:shadow-gray-800 absolute -top-10 right-0'
+              onClick={e => {
+                e.preventDefault()
+                resetForm()
+              }}
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-6 w-6'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15'
                 />
               </svg>
             </button>
